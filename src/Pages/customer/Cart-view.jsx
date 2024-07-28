4e6bd138cd38773c386e1b8470b16efbdba1958b
Satvickk <lenovo37819@gmail.com>
2024-07-29 00:02:51 +0530

    deployment fix

--- src/Pages/customer/Cart-view.jsx
+++ src/Pages/customer/Cart-view.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Header = React.lazy(() => import("../Components/Header/Header"));
-const Footer = React.lazy(() => import("../Components/Footer/Footer"));
-const CartLayout = React.lazy(() => import("../Components/Cart/CartLayout"))
+const Header = React.lazy(() => import("../../Components/Header/Header"));
+const Footer = React.lazy(() => import("../../Components/Footer/Footer"));
+const CartLayout = React.lazy(() => import("../../Components/Cart/CartLayout"))
 
 export default function CartView(){
   
